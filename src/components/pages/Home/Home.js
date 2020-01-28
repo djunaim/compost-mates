df89cb36d598bb6f3b8f1bab40707f7284e25641
Monica Djunaidi <modjun12@gmail.com>
2020-01-28 13:09:21 -0600

    wip

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import './Home.scss';
+
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home</h1>
+        <div className="parallax"></div>
+        <div><img url="" /></div>
+        <div className="toCompost">To Compost</div>
+        <div className="notCompost">Not Compost</div>
+        <div className="parallax"></div>
       </div>
     );
   }
