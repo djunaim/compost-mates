067f9dba55cb48f081c69a93259d59cabe084054
Monica Djunaidi <modjun12@gmail.com>
2020-01-28 14:16:25 -0600

    have carousel and parallax

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 import './Home.scss';
 
@@ -6,9 +7,35 @@ class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <div className="parallax"></div>
-        <div><img url="" /></div>
+        {/* <div className="parallax"></div> */}
+        <div className="carousel">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://raw.githubusercontent.com/djunaim/compost-mates/master/src/components/pages/Home/assets/horizontal-black.png"
+                alt="horizontal compost mates logo"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1508500351687-8c9e0a4e35f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
+                alt="single leaf"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1545165311-508ed0c91361?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80"
+                alt="vegetable"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        {/* <div className="parallax"></div> */}
         <div className="toCompost">To Compost</div>
+        <div className="parallax"></div>
         <div className="notCompost">Not Compost</div>
         <div className="parallax"></div>
       </div>
