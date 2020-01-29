0b94c106e7fc9462b5c2c40a2ea61960f3488826
Monica Djunaidi <modjun12@gmail.com>
2020-01-28 20:29:53 -0600

    my compost styled

--- src/components/shared/Compost/Compost.js
+++ src/components/shared/Compost/Compost.js
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import compostShape from '../../../helpers/propz/compostShape';
 
+import './Compost.scss';
+
 class Compost extends React.Component {
   static propTypes = {
     compost: compostShape.compostShape,
@@ -21,12 +23,14 @@ class Compost extends React.Component {
     const { compost } = this.props;
     return (
       <div className="Compost col-md-4">
-        <div className="card border-dark mb-3">
+        <div className="card border-dark mb-3 compostCard">
           <div className="card-body">
-            <h4><strong>{compost.name}</strong></h4>
-            <Link className="btn btn-outline-dark" to={`/compost/${compost.id}`}>View</Link>
-            <Link className="btn" to={`/compost/${compost.id}/edit`}><FontAwesomeIcon icon={faPen} /></Link>
-            <button className="btn" onClick={this.deleteCompostEvent}><FontAwesomeIcon icon={faTrash} /></button>
+            <h4 className="card-title"><strong>{compost.name}</strong></h4>
+            <div className="compostButtons">
+              <Link className="btn btn-outline-dark" to={`/compost/${compost.id}`}>View</Link>
+              <Link className="btn" to={`/compost/${compost.id}/edit`}><FontAwesomeIcon icon={faPen} /></Link>
+              <button className="btn" onClick={this.deleteCompostEvent}><FontAwesomeIcon icon={faTrash} /></button>
+            </div>
           </div>
         </div>
       </div>
