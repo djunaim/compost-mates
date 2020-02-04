cedac40229b6df9eb0885330e1cd75be5f952098
Monica Djunaidi <modjun12@gmail.com>
2020-02-03 19:47:33 -0600

    took out hamburger in navbar

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -77,11 +77,11 @@ class MyNavbar extends React.Component {
       <div className="MyNavbar">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <Link className="navbar-brand compostMatesBrand" to="/"><img src="https://raw.githubusercontent.com/djunaim/compost-mates/master/src/components/shared/MyNavbar/assets/square-black.png" alt="logo-square" /></Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-          </button>
+          </button> */}
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div id="navbarSupportedContent">
             {
               buildNavbar()
             }
